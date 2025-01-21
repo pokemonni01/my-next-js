77b726b85f66e1122edc1a0ca145c4d12c673740
Wachirapong Prasertwong <ni@oozou.com>
2025-01-21 15:43:03 +0700

    Outputting list data

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -23,9 +23,19 @@ const PostList: React.FC<PostListProps> = ({ isPosting, onStopPosting }) => {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Paul" body="Hi, there!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post, index) => (
+            <Post key={index} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 };
