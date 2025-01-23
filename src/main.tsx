d50e00f50955aff474a4a7b8e442e7ba8d35cfd1
Wachirapong Prasertwong <ni@oozou.com>
2025-01-23 17:33:58 +0700

    Working with layout routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import NewPost from "./components/NewPost.tsx";
+import RootLayout from "./routes/RootLayout.tsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/create-post", element: <NewPost /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
